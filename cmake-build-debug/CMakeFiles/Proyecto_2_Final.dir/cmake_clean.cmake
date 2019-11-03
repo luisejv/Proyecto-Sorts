ed8419101ab892623c085be16dde90f31cd131f7
luisejv <38704851+luisejv@users.noreply.github.com>
2019-11-03 18:57:29 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Proyecto_2_Final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_2_Final.dir/main.cpp.obj"
+  "Proyecto_2_Final.exe"
+  "Proyecto_2_Final.exe.manifest"
+  "Proyecto_2_Final.pdb"
+  "libProyecto_2_Final.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_2_Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
